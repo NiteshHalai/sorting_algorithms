9cc0bb1cd5e5de8f4e4556c2148e5f06dee89fa0
NiteshHalai <nitesh.halai@gmail.com>
2023-01-28 21:58:55 +0300

    Create 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,22 @@
+void shell_sort(int *array, size_t size) {
+    int gap = 1, temp, i, j;
+
+    while (gap < size)
+        gap = gap * 3 + 1;
+
+    while (gap > 1) {
+        gap = (gap - 1) / 3;
+        for (i = gap; i < size; i++) {
+            temp = array[i];
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                array[j] = array[j - gap];
+            }
+            array[j] = temp;
+        }
+
+        for (int i = 0; i < size; i++)
+            printf("%d ", array[i]);
+
+        printf("\n");
+    }
+}
